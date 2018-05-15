c2dfe4b735420f92a814394fbc590a59c29fdee4
John Guberman <jeguberman@gmail.com>
2018-05-15 14:05:56 -0400

    refactoring

--- util/generate_response_body_and_status_code.rb
+++ util/generate_response_body_and_status_code.rb
@@ -11,17 +11,18 @@ def generate_response_body_and_status_code(package) #these should be broken down
   if path == "timeout!" #if the server never completed reading the get
     buffer = render_html(title:"408 Timeout", banner: "408 Timeout", message: "The server timed out before it processed your request")
     status_code = 408
-
-  elsif File.split(path).last == "index.html" #if the server was told to return a directory
-    buffer = render_html(title: "index", banner: "index")
-    status_code = 200
-
+    
   elsif File.exist?(path) && !File.directory?(path) #if the server was told to serve a specific file
     #respond with a 200 ok status code to indicate christmas is saved
     File.open(path, "rb") do |file|
       buffer = file.read
     end
     status_code = 200
+
+  elsif File.split(path).last == "index.html" #if the server was told to return a directory
+    buffer = render_html(title: "index", banner: "index")
+    status_code = 200
+
 
   else  # if the server was told to serve a file that doesn't exist
     buffer = render_html(title: "404 Not Found", banner: "404 not found", message: "The file was not found by the server")
--- util/get_host_address.rb
+++ util/get_host_address.rb
@@ -1,5 +1,8 @@
 def get_host_address_ipv4 #returns first listed ipv4 address for current host which isn't localhost
-  Socket.ip_address_list.detect do |addrinfo|
+  #if [no wifi]
+  # return #local host
+# end
+  Socket.ip_address_list.detect do |addrinfo| #clean this up, make it clearer
     addrinfo.ipv4? &&
     addrinfo.ip_address != "127.0.0.1"
   end.ip_address
